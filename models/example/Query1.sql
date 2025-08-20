669c15ca59826250364a73404d3068411a172fcc
Ranjit Pandey <pandeyranjit2139@gmail.com>
2025-08-20 17:29:00 +0530

    Add new query

--- models/example/Query1.sql
+++ models/example/Query1.sql
@@ -4,5 +4,9 @@
 -- select * from Jobs_details
 -- where employee_residence like 'India' or employee_residence like 'China' and salary >= 50000
 
-select * from Jobs_details
-where years_experience >= 10 
+-- select * from Jobs_details
+-- where years_experience >= 10 
+
+select COUNT(Job_ID) as No_of_Employee,employee_residence as Country
+from jobs_details
+group by employee_residence

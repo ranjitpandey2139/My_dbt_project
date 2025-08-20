6f09733a306065100325370adf74b4cbba5e908a
Ranjit Pandey <pandeyranjit2139@gmail.com>
2025-08-20 16:49:04 +0530

    change query

--- models/example/Query1.sql
+++ models/example/Query1.sql
@@ -1,6 +1,8 @@
 {{ config(materialized='table', schema = 'Dbt1') }}
 
 
-select * from Jobs_details
-where employee_residence like 'India' or employee_residence like 'China' and salary >= 50000
+-- select * from Jobs_details
+-- where employee_residence like 'India' or employee_residence like 'China' and salary >= 50000
 
+select * from Jobs_details
+where years_experience >= 10 

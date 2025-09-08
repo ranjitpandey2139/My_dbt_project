bca796061d90cf91a52ae668bbb8224484e514f2
Ranjit Pandey <pandeyranjit2139@gmail.com>
2025-09-08 12:36:58 +0530

    Sum_Total_salary_cuntry_based

--- models/example/My_query.sql
+++ models/example/My_query.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='table', schema = 'All_China_Employee') }}
+{{ config(materialized='table', schema = 'Sum_salary_cuntry_based') }}
 
 -- select
 --    *
@@ -8,5 +8,12 @@
 -- select * from jobs_details
 -- where employee_residence like 'China'
 
-select * from jobs_details
-where employee_residence like 'France'
+-- select * from jobs_details
+-- where employee_residence like 'France'
+
+
+SELECT
+    SUM(CASE WHEN employee_residence = 'India' THEN salary ELSE 0 END) AS Total_indian_salary,
+    SUM(CASE WHEN employee_residence = 'China' THEN salary ELSE 0 END) AS Total_china_salary,
+    SUM(CASE WHEN employee_residence = 'United States' THEN salary ELSE 0 END) AS Total_united_states_salary
+FROM Jobs_details;
